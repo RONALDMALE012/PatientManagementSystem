9b88bd9250e1aa724fa45211324f4b5935666e98
Ronald <ronaldmale012@gmail.com>
2026-08-28 17:59:40 +1400

    Fix ALLOWED_HOSTS for Render

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'False'
 
-ALLOWED_HOSTS = ["patient_management.com",]
+ALLOWED_HOSTS = ["://onrender.com", 'localhost', '127.0.0.1']
 
 
 # Application definition
